2b15f62277c4f2071ae13e08beb6c0d132f129d3
Ryan Currington <ryan.currington@glew.io>
2015-08-27 15:16:35 -0400

    add primary id to order item

--- app/code/community/Glew/Service/Model/Types/OrderItem.php
+++ app/code/community/Glew/Service/Model/Types/OrderItem.php
@@ -4,6 +4,7 @@ class Glew_Service_Model_Types_OrderItem
 {
     public function parse($orderItem)
     {
+        $this->order_item_id = $orderItem->getId();
         $this->order_id = $orderItem->getOrderId();
         $this->created_at = $orderItem->getCreatedAt();
         $this->updated_at = $orderItem->getUpdatedAt();
