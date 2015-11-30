087e8999f8b7dfbc626f9220886354b068da8d62
Ryan Currington <ryan.currington@glew.io>
2015-11-30 09:36:21 -0500

    add cost and price to inventory

--- app/code/community/Glew/Service/Model/Types/InventoryItem.php
+++ app/code/community/Glew/Service/Model/Types/InventoryItem.php
@@ -1,14 +1,17 @@
-<?php 
+<?php
 
 class Glew_Service_Model_Types_InventoryItem
 {
     public function parse($item)
     {
+      $product = Mage::getModel('catalog/product')->load($item->getProductId());
     	$this->id = $item->getItemId();
     	$this->product_id = $item->getProductId();
-        $this->qty = $item->getQty();
-        
-        return $this;
+      $this->qty = $item->getQty();
+      $this->price = $product->getPrice();
+      $this->cost = $product->getCost();
+
+      return $this;
     }
-    
+
 }
